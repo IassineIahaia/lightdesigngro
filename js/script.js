b4bb377afa154d661545a785717e16b6da042654
Iassine Iahaia <zinhocabila58@gmail.com>
2025-01-24 10:08:05 +0200

    fix bug menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 function fixHeaderOnScroll() {
     const header = document.querySelector("header");
   
-    if (window.scrollY >= 1) {
+    // Adiciona ou remove a classe 'fixed' com base na posição do scroll
+    if (window.scrollY > 0) {
       header.classList.add("fixed");
     } else {
       header.classList.remove("fixed");
     }
   }
+  
+  // Adiciona o evento de scroll
+  window.addEventListener("scroll", fixHeaderOnScroll);
+  
+  // Executa a função uma vez ao carregar a página
+  fixHeaderOnScroll();
+  
